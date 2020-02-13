52cb86ba1c741c6a469643d0839c716ca679014e
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2020-02-13 14:27:32 +0300

    - Added Double cast test cases - Updated frameworks

--- Example/Tests/TypeCastSpec.swift
+++ Example/Tests/TypeCastSpec.swift
@@ -48,6 +48,55 @@ class TypeCastSpec: QuickSpec {
                     expect(model?.stringEnum).to(equal(.two))
                 }
             }
+            
+            context("when JSON contains doubles") {
+                let typeMismatchingJSON: [String: Any] = [
+                    "double1": 1,
+                    "double2": "1.0",
+                    "double3": "-1.0000",
+                    "double4": "1.0001",
+                    "double5": "-100.001",
+                    "double6": "1000000.0001",
+                    "double7": "-2.66",
+                    "double8": -1000000,
+                ]
+                
+                it("should map it propertly") {
+                    let model = MyDoubleModel(JSON: typeMismatchingJSON)
+                    expect(model?.double1).to(equal(1))
+                    expect(model?.double2).to(equal(1))
+                    expect(model?.double3).to(equal(-1))
+                    expect(model?.double4).to(equal(1.0001))
+                    expect(model?.double5).to(equal(-100.001))
+                    expect(model?.double6).to(equal(1000000.0001))
+                    expect(model?.double7).to(equal(-2.66))
+                    expect(model?.double8).to(equal(-1000000))
+                }
+            }
         }
+    }
+}
+
+private struct MyDoubleModel: Mappable {
+    var double1: Double?
+    var double2: Double?
+    var double3: Double?
+    var double4: Double?
+    var double5: Double?
+    var double6: Double?
+    var double7: Double?
+    var double8: Double?
+    
+    init?(map: Map) {}
+    
+    mutating func mapping(map: Map) {
+        double1 <- (map["double1"], TypeCastTransform())
+        double2 <- (map["double2"], TypeCastTransform())
+        double3 <- (map["double3"], TypeCastTransform())
+        double4 <- (map["double4"], TypeCastTransform())
+        double5 <- (map["double5"], TypeCastTransform())
+        double6 <- (map["double6"], TypeCastTransform())
+        double7 <- (map["double7"], TypeCastTransform())
+        double8 <- (map["double8"], TypeCastTransform())
     }
 }
