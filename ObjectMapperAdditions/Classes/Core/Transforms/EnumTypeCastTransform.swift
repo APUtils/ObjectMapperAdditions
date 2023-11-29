46dd6630a399e4d91677c049ec74e29d1cb8a0b4
Anton Plebanovich <anton.plebanovich@gmail.com>
2023-11-29 14:50:07 +0600

    - [EnumTypeCastTransform] Report unknown cases

--- ObjectMapperAdditions/Classes/Core/Transforms/EnumTypeCastTransform.swift
+++ ObjectMapperAdditions/Classes/Core/Transforms/EnumTypeCastTransform.swift
@@ -17,24 +17,39 @@ public class EnumTypeCastTransform<T: RawRepresentable>: TransformType {
     public init() {}
     
     open func transformFromJSON(_ value: Any?) -> T? {
-        if value == nil {
-            return nil
-        } else if let value = value as? T.RawValue {
-            return T(rawValue: value)
+        guard let value else { return nil }
+        
+        if let value = value as? T.RawValue {
+            let result = T(rawValue: value)
+            reportUnknownCaseIfNeeded(result, value: value, rawValue: nil)
+            return result
+            
         } else if T.RawValue.self == Int.self {
             let rawValue = IntTransform.shared.transformFromJSON(value) as? T.RawValue
-            return rawValue.flatMap(T.init(rawValue:))
+            let result = rawValue.flatMap(T.init(rawValue:))
+            reportUnknownCaseIfNeeded(result, value: value, rawValue: rawValue)
+            return result
+            
         } else if T.RawValue.self == Double.self {
             let rawValue = DoubleTransform.shared.transformFromJSON(value) as? T.RawValue
-            return rawValue.flatMap(T.init(rawValue:))
+            let result = rawValue.flatMap(T.init(rawValue:))
+            reportUnknownCaseIfNeeded(result, value: value, rawValue: rawValue)
+            return result
+            
         } else if T.RawValue.self == Bool.self {
             let rawValue = BoolTransform.shared.transformFromJSON(value) as? T.RawValue
-            return rawValue.flatMap(T.init(rawValue:))
+            let result = rawValue.flatMap(T.init(rawValue:))
+            reportUnknownCaseIfNeeded(result, value: value, rawValue: rawValue)
+            return result
+            
         } else if T.RawValue.self == String.self {
             let rawValue = StringTransform.shared.transformFromJSON(value) as? T.RawValue
-            return rawValue.flatMap(T.init(rawValue:))
+            let result = rawValue.flatMap(T.init(rawValue:))
+            reportUnknownCaseIfNeeded(result, value: value, rawValue: rawValue)
+            return result
+            
         } else {
-            RoutableLogger.logError("Can not cast value of type \(type(of: value!)) to type \(T.RawValue.self)", data: ["value": value])
+            RoutableLogger.logError("Can not cast value of type \(type(of: value)) to type \(T.RawValue.self)", data: ["value": value])
             return nil
         }
     }
@@ -44,5 +59,11 @@ public class EnumTypeCastTransform<T: RawRepresentable>: TransformType {
             return obj.rawValue
         }
         return nil
+    }
+    
+    private func reportUnknownCaseIfNeeded(_ result: T?, value: Any, rawValue: Any?) {
+        if result == nil {
+            RoutableLogger.logError("Unknown enum case", data: ["value": value, "rawValue": rawValue])
+        }
     }
 }
