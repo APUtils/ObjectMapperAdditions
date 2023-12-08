ee60cdc8248e725c6a62c09dd84dbe5636537bef
Anton Plebanovich <anton.plebanovich@gmail.com>
2023-12-08 18:22:47 +0600

    - Shorter syntax

--- ObjectMapperAdditions/Classes/Core/Extensions/Map+Additions.swift
+++ ObjectMapperAdditions/Classes/Core/Extensions/Map+Additions.swift
@@ -10,19 +10,19 @@ import ObjectMapper
 
 public extension Map {
     
-    func boolValue(key: String) -> Bool? {
+    func boolValue(_ key: String) -> Bool? {
         try? value(key, using: BoolTransform.shared)
     }
     
-    func doubleValue(key: String) -> Double? {
+    func doubleValue(_ key: String) -> Double? {
         try? value(key, using: DoubleTransform.shared)
     }
     
-    func intValue(key: String) -> Int? {
+    func intValue(_ key: String) -> Int? {
         try? value(key, using: IntTransform.shared)
     }
     
-    func stringValue(key: String) -> String? {
+    func stringValue(_ key: String) -> String? {
         try? value(key, using: StringTransform.shared)
     }
     
